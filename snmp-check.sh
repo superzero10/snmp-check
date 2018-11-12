fddd5d1a0bc583957f3b7937f85f8ceb4a2de541
root <root@localhost.localdomain>
2018-11-12 16:50:43 +0100

    fix

--- snmp-check.sh
+++ snmp-check.sh
@@ -4,7 +4,7 @@
 
 if [ $# -le 0 ]; then
 	echo "
-Usage : ./snmp_check [OPTION][HOST]
+Usage : ./snmp_check [C_KEY][OPTION][HOST]
 OPTION:
 --local :	test localhost snmp
 -h 	:	Test one host, who's the next argument
@@ -15,20 +15,20 @@ fi
 
 #start
 
-if [ $1 = "--local" ]
+if [ $2 = "--local" ]
 then
-	snmpwalk -v 2c -c EyesOfNetwork -O e 127.0.0.1 | head
+	snmpwalk -v 2c -c $1 -O e 127.0.0.1 | head
 else
-	if [ $1 = "-h" ]
+	if [ $2 = "-h" ]
 	then
-		snmpwalk -v 2c -c EyesOfNetwork -O e $2 | head
+		snmpwalk -v 2c -c $1 -O e $3 | head
 	else
-		if [ $1 = "--list" ]
+		if [ $2 = "--list" ]
 		then
 			while read host; do
 				echo $host
-				snmpwalk -v 2c -c EyesOfNetwork -O e $host | head
-			done < $2
+				snmpwalk -v 2c -c $1 -O e $host | head
+			done < $3
 		else
 			echo "error" 1>&2
 			exit
